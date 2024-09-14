3c5930fda6173ccff0c4bdf2fa1d8770db8b0621
Shreyas <117995509+Philomath21@users.noreply.github.com>
2024-09-14 18:26:05 +0530

    implement #insert(element)

--- lib/binary_search_trees.rb
+++ lib/binary_search_trees.rb
@@ -3,7 +3,7 @@ class Node
 
   attr_accessor :data, :left_child, :right_child
 
-  def initialize (data)
+  def initialize(data)
     self.data = data
     self.left_child = nil
     self.right_child = nil
@@ -13,19 +13,19 @@ end
 class Tree
   attr_accessor :array, :root
 
-  def initialize (array)
+  def initialize(array)
     self.array = array
     self.root = nil
   end
 
-  def build_tree (sub_a)
+  def build_tree(sub_a)
     sorted_a = sub_a.sort
     mid_index = sorted_a.length / 2
     self.root = Node.new(sorted_a[mid_index])
 
     if sorted_a.length > 1
-      self.root.left_child = build_tree(sorted_a[..mid_index-1])
-      self.root.right_child = build_tree(sorted_a[mid_index+1..])
+      root.left_child = build_tree(sorted_a[..mid_index - 1])
+      root.right_child = build_tree(sorted_a[mid_index + 1..])
     end
     root
   end
@@ -36,6 +36,21 @@ class Tree
     pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 
-  def ins
+  def insert(element)
+    check_root = root
+    check_child = "element == node data, I can't handle this yet!"
 
+    loop do
+      check_child = check_root.left_child if element < check_root.data
+      check_child = check_root.right_child if element > check_root.data
+
+      return check_child = Node.new(element) if check_child.nil?
+      return puts check_child if check_child in String
+
+      check_root = check_child
+    end
+  end
+
+  def delete(element)
+  end
 end
